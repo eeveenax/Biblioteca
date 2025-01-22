ce7b5892ba3b7399cc70c45387261499583e0f5d
eeveenax <eveliagilparedes@gmail.com>
2025-01-22 17:07:28 +0100

    cambios

--- src/GestorUsuario.java
+++ src/GestorUsuario.java
@@ -1,3 +1,19 @@
 public class GestorUsuario {
 
+    private Usuario[] listaUsuario;
+    private int contadorUsuario;
+    private int tamUser= 500;
+ 
+
+
+    
+     
+       
+
+    
 }
+
+/*
+ * Registrar nuevos usuarios.(admin)
+ * Consultar información de usuarios registrados.(admin)
+ */
