e82220ea87db98d51f38b866f39779911ce7508c
Mery <mceballosm04@iesalbarregas.es>
2025-01-22 17:13:02 +0100

    cambio

--- src/GestorLibros.java
+++ src/GestorLibros.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+
 public class GestorLibros {
 
+    private Libro[] libros;
+    private int lleno;
+    private static final int TAM = 100;
+    }
+
+public GestorLibros() {
+    libros = new Libro[TAM];
+    lleno = 0;
+
 }
+
+public void nuevoLibro
+
+
